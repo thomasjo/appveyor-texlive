2685a1be0c9355b3de9810c6d2a217cf53111406
Thomas Johansen <thomasjo@gmail.com>
2014-08-04 23:21:55 +0200

    Initial commit

---
+++ download.ps1
@@ -0,0 +1,23 @@
+﻿$TickMark = [Char]0x2713
+
+function Expand-ZipFile($FilePath, $DestinationPath) {
+  New-Item -ItemType Directory -Path $DestinationPath -WarningAction SilentlyContinue -Force | Out-Null
+
+  $Shell = New-Object -ComObject Shell.Application
+  $ZipFile = $Shell.Namespace($FilePath)
+
+  # vOption flags: http://msdn.microsoft.com/en-us/library/windows/desktop/bb787866(v=vs.85).aspx
+  $Shell.Namespace($DestinationPath).CopyHere($ZipFile.Items(), 0x14) | Out-Null
+}
+
+$DownloadUrl = "http://mirror.ctan.org/systems/texlive/tlnet/install-tl.zip"
+$FilePath = "$PSScriptRoot\install-tl.zip"
+$DestinationPath = "$PSScriptRoot"
+
+Write-Host "Downloading $DownloadUrl... " -NoNewline
+(New-Object Net.WebClient).DownloadFile($DownloadUrl, $FilePath)
+Write-Host $TickMark
+
+Write-Host "Extracting $FilePath... " -NoNewline
+Expand-ZipFile -FilePath $FilePath -DestinationPath $DestinationPath
+Write-Host $TickMark
---
+++ prepare.ps1
@@ -0,0 +1,48 @@
+$TargetDir = "$PSScriptRoot\texlive"
+Remove-Item -Path $TargetDir -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -Force
+
+$Date = ((Get-Date).ToUniversalTime()).ToString("ddd MMM  d HH:mm:ss yyyy UTC")
+$PosixTargetDir = $TargetDir -replace "\\","/"
+
+$ProfilePath = "$PSScriptRoot\texlive.profile"
+@"
+# texlive.profile written on $Date
+# It will NOT be updated and reflects only the
+# installation profile at installation time.
+selected_scheme scheme-custom
+TEXDIR $PosixTargetDir
+TEXMFCONFIG `$TEXMFSYSCONFIG
+TEXMFHOME `$TEXMFLOCAL
+TEXMFLOCAL $PosixTargetDir/texmf-local
+TEXMFSYSCONFIG $PosixTargetDir/texmf-config
+TEXMFSYSVAR $PosixTargetDir/texmf-var
+TEXMFVAR `$TEXMFSYSVAR
+binary_win32 1
+collection-basic 1
+collection-binextra 1
+collection-latex 1
+collection-latexrecommended 1
+collection-wintools 1
+option_adjustrepo 1
+option_autobackup 0
+option_desktop_integration 0
+option_doc 0
+option_file_assocs 0
+option_fmt 1
+option_letter 0
+option_menu_integration  0
+option_path 0
+option_post_code 1
+option_src 1
+option_sys_bin /usr/local/bin
+option_sys_info /usr/local/share/info
+option_sys_man /usr/local/share/man
+option_w32_multi_user 0
+option_write18_restricted 1
+portable 1
+"@ > $ProfilePath
+
+# TODO:
+# - Run install-tl-windows.bat with the generated profile.
+# - `rm -r $TARGET_DIR/texmf-dist/doc`
+# - Compress portable install using 7za.exe

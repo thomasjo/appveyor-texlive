691f83d94fbcdf5d9b1d26c2bdfd065394877d10
Thomas Johansen <thomasjo@gmail.com>
2014-08-05 08:27:32 +0200

    Add 7-zip packaging script

--- prepare.ps1
+++ prepare.ps1
@@ -19,7 +19,7 @@ binary_win32 1
 collection-basic 1
 collection-binextra 1
 collection-latex 1
-collection-latexrecommended 1
+collection-latexrecommended 0
 collection-wintools 1
 in_place 0
 option_adjustrepo 1
@@ -32,7 +32,7 @@ option_letter 0
 option_menu_integration  0
 option_path 0
 option_post_code 1
-option_src 1
+option_src 0
 option_sys_bin /usr/local/bin
 option_sys_info /usr/local/share/info
 option_sys_man /usr/local/share/man
@@ -45,7 +45,9 @@ $PosixProfilePath = $ProfilePath -replace "\\","/"
 $InstallScriptPath = (Get-ChildItem .\install-tl-*\install-tl-windows.bat).FullName
 Invoke-Expression "$InstallScriptPath -profile $PosixProfilePath"
 
-# TODO:
-# - [x] Run install-tl-windows.bat with the generated profile.
-# - [ ] `rm -r $TARGET_DIR/texmf-dist/doc`
-# - [ ] Compress portable install using 7za.exe
+Remove-Item -Path "$TargetDir\texmf-dist\doc" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -Recurse -Force
+Remove-Item -Path "$TargetDir\texmf-dist\source" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -Recurse -Force
+
+$PackagePath = "$PSScriptRoot\texlive.7z"
+Remove-Item -Path $PackagePath -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -Force
+Invoke-Expression "7za a -mx9 $PackagePath $TargetDir"
